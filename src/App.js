ccb38463722739a2dc837f5a61a24f2ed259ba44
Razvan Celea <razvan.celea.firefly@gmail.com>
2020-07-08 21:28:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import './App.css';
-
-function App() 
-{
-
-  function upload(){
-    var input=document.getElementById("upload");
-    var freader=new FileReader();
-    freader.readAsDataURL(input.files[1]);
-    freader.onload=function(){
-document.getElementById("video").src=freader.result;
-}
-  }
-
-function start(){
-
-  document.getElementById("video").play();
-}
-
-  
-return(
-  <div   >
-    <div >
-<video width="720" height="480" controls="controls" id="video">
-<source id="src"/>
-  </video>
-  </div>
-<input type="file" accept="video*/" id="upload" onChange={upload()}/>
-<button id="start" onClick={start()}>Start</button>
-
-  </div>
-  
-  )
-
-=======
 import React from "react";
 import "./App.css";
 
@@ -66,7 +29,6 @@ function App() {
       <button>Start</button>
     </div>
   );
->>>>>>> first commit
 }
 
 export default App;
