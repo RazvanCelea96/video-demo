e3a5c02ce936717fd76dc294729f0485693a8e55
Razvan Celea <razvan.celea.firefly@gmail.com>
2020-07-08 21:26:04 +0300

    first commit
    Added video upload feature and play

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import './App.css';
 
@@ -33,6 +34,39 @@ return(
   
   )
 
+=======
+import React from "react";
+import "./App.css";
+
+function App() {
+  function upload() {
+    var input = document.getElementById("upload");
+    var freader = new FileReader();
+    freader.readAsDataURL(input.files[0]);
+    freader.onload = function () {
+      document.getElementById("video").src = freader.result;
+    };
+  }
+
+  function download() {}
+
+  return (
+    <div>
+      <video width="720" height="480" controls="controls" id="video">
+        <source id="src" type="video/mp4" />
+      </video>
+
+      <div>
+        <input type="file" accept="video/*" id="upload" onChange={upload} />
+
+        <button id="src" onClick={download}>
+          Download
+        </button>
+      </div>
+      <button>Start</button>
+    </div>
+  );
+>>>>>>> first commit
 }
 
 export default App;
